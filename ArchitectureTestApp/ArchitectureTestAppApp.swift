30f5c8c9e15be4f8dc6768bfc24e151048e9ea68
윤병은 <95ybya@gmail.com>
2025-02-18 15:47:22 +0900

    Initial Commit

---
+++ ArchitectureTestApp/ArchitectureTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArchitectureTestAppApp.swift
+//  ArchitectureTestApp
+//
+//  Created by Yun on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ArchitectureTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
